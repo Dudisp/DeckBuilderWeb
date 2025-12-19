8d0ac104421372eb0b4c9761f1fd605c3f739a78
Dudi Spivak <dudisp@gmail.com>
2025-12-19 09:08:42 +0200

    removed unused files

--- proxy.py
+++
@@ -1,8 +0,0 @@
-PROXY_URL = "https://proxylist.geonode.com/api/proxy-list?limit=500&page=1&sort_by=lastChecked&sort_type=desc"
-PROXY = "67.43.228.251"
-
-proxies = {
-   'http': 'http://67.43.228.251:8347',
-}
-
-
